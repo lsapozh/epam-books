a59b8635c2f34f4d2f5fa0043ada57c342c07d6e
liliiasapozhnikova <sapozhnikova111@gmail.com>
2017-12-16 23:59:01 +0400

    added validation refactoring

--- js/books.js
+++ js/books.js
@@ -15,32 +15,32 @@ function initBooks() {
         authors: "Stoyan Stefanov",
         year: 2008,
         image: "https://images-na.ssl-images-amazon.com/images/I/51%2BSiphz7AL._SX377_BO1,204,203,200_.jpg"
+    }, {
+        name: "JavaScript: The Definitive Guide: Activate Your Web Pages (Definitive Guides)",
+        authors: "David Flanagan",
+        year: 2011,
+        image: "https://images-na.ssl-images-amazon.com/images/I/51WD-F3GobL._SX379_BO1,204,203,200_.jpg"
     }];
 }
 
 function paintListOfBooks() {
     document.getElementById("books-list").innerHTML = "";
     books.forEach((currentBook) => {
-        var book = document.createElement('div');
+        const book = document.createElement('div');
         book.className = "book container";
-        book.innerHTML = "" +
-            "<img src=\"" + currentBook.image + "\" class=\"book-image\">\n" +
-            "<div class=\"info-about-book\">\n" +
-            "    <p class=\"book-name\">" + currentBook.name + "</p>\n" +
-            "    <p class=\"book-authors\">" + currentBook.authors + "</p>\n" +
-            "    <p class=\"book-year\">" + currentBook.year+ "</p>\n" +
-            "</div>\n" +
-            "<div class=\"book-buttons\">\n" +
-            "    <button class=\"btn-update btn-cyan\" id=\"btn-update-book-" + books.indexOf(currentBook) + "\"\>" +
-            "       <span class=\"btn-text\">Изменить</span>" +
-            "       <i class=\"fa fa-pencil-square-o icons\" aria-hidden=\"true\"></i>" +
-            "    </button>\n" +
-            "    <button class=\"btn-delete btn-red\" id=\"btn-delete-book-" + books.indexOf(currentBook)+ "\"\>" +
-            "       <span class=\"btn-text\">Удалить</span>" +
-            "       <i class=\"fa fa-times icons\" aria-hidden=\"true\"></i>" +
-            "</button>\n" +
-            "</div>" +
-            "<div class=\"clear\"></div>";
+        book.innerHTML = `
+            <img src="${currentBook.image}" class="book-image">
+            <div class="info-about-book">
+                <p class="book-name">${currentBook.name}</p>
+                <p class="book-authors">${currentBook.authors}</p>
+                <p class="book-year">${currentBook.year}</p>
+            </div>
+            <div class="book-buttons">
+                <button class="btn-update btn-cyan" id="btn-update-book-${books.indexOf(currentBook)}">       <span class="btn-text">Изменить</span>       <i class="fa fa-pencil-square-o icons" aria-hidden="true"></i>    </button>
+                <button class="btn-delete btn-red" id="btn-delete-book-${books.indexOf(currentBook)}">       <span class="btn-text">Удалить</span>       <i class="fa fa-times icons" aria-hidden="true"></i></button>
+            </div>
+            <div class="clear"></div>
+        `;
         document.getElementById("books-list").appendChild(book);
     });
 }
@@ -59,19 +59,30 @@ function returnToBooksList() {
     show("books-list");
 }
 
-function isCorrectValue(year) {
+function isCorrectYear(year) {
     if (year <= 2017 && year >= 0) {
         return true;
-    } else return false;
+    } else {
+        return false;
+    }
 }
 
+function isCorrectName(name) {
+    return !!name;
+}
+
+function hideError() {
+    hide("adding-error-name");
+    hide("updating-error-name");
+    hide("adding-error-year");
+    hide("updating-error-year");
+}
 
 function showAddingPage() {
     hide("books-list");
     hide("updating-book");
     show("adding-new-book");
-    hide("adding-error");
-    hide("updating-error");
+    hideError();
     document.getElementById("name-input").value = null;
     document.getElementById("authors-input").value = null;
     document.getElementById("year-input").value = null;
@@ -83,23 +94,30 @@ function saveNewBook() {
     let authors = document.getElementById("authors-input").value;
     let year = document.getElementById("year-input").value;
     let image = document.getElementById("image-input").value;
-    if(isCorrectValue(year)) {
+    if (!isCorrectYear(year)) {
+        show("adding-error-year");
+        hide("adding-error-name");
+    } else if (!isCorrectName(name)) {
+        show("adding-error-name");
+        hide("adding-error-year");
+    } else {
+        if (!image) {
+            image = standardImage;
+        }
         books.push({name: name, authors: authors, year: year, image: image});
         paintListOfBooks();
         switchOnListenerForList();
         returnToBooksList();
         saveToLocalStorage();
-    } else {
-        show("adding-error");
     }
 }
+
 
 function showUpdatingPage(id) {
     hide("books-list");
     hide("adding-new-book");
     show("updating-book");
-    hide("adding-error");
-    hide("updating-error");
+    hideError();
     let book = books[id];
     document.getElementById("name-input-updated").value = book.name;
     document.getElementById("authors-input-updated").value = book.authors;
@@ -108,17 +126,27 @@ function showUpdatingPage(id) {
 }
 
 function saveUpdatedBook(id) {
-    let name = document.getElementById("name-input-updated").value;
-    let authors = document.getElementById("authors-input-updated").value;
-    let year = document.getElementById("year-input-updated").value;
+    const name = document.getElementById("name-input-updated").value;
+    const authors = document.getElementById("authors-input-updated").value;
+    const year = document.getElementById("year-input-updated").value;
     let image = document.getElementById("image-input-updated").value;
-    if (isCorrectValue(year)) {
+
+    if (!isCorrectYear(year)) {
+        show("updating-error-year");
+        hide("updating-error-name");
+    } else if (!isCorrectName(name)) {
+        show("updating-error-name");
+        hide("updating-error-year");
+    } else {
+        if (!image) {
+            image = standardImage;
+        }
         books[id] = {name: name, authors: authors, year: year, image: image};
         paintListOfBooks();
         switchOnListenerForList();
         returnToBooksList();
         saveToLocalStorage();
-    } else show("updating-error");
+    }
 }
 
 function deleteBook(id) {
@@ -131,7 +159,7 @@ function deleteBook(id) {
 
 function addScrollListener() {
     let showButton = false;
-    window.addEventListener('scroll', function (e) {
+    window.addEventListener('scroll', function () {
         if (window.scrollY > 80) {
             if (!showButton) {
                 showButton = true;
@@ -188,7 +216,7 @@ function saveToLocalStorage() {
 
 
 let books = [];
-// localStorage.clear();
+//localStorage.clear(); // uncomment to clear storage
 if (localStorage.getItem("books")) {
     books = JSON.parse(localStorage.getItem("books"));
 } else {
@@ -196,6 +224,7 @@ if (localStorage.getItem("books")) {
 }
 
 let currentBookIndex;
+const standardImage = "https://i.pinimg.com/736x/fb/ab/7a/fbab7ad8cf7c407c40a66a7fafd4c933--clipart-black-and-white-open-book.jpg";
 paintListOfBooks();
 switchOnListeners();
 switchOnListenerForList();
